e8329ea7ff2798b94b0cf32eadab0faa014d66ed
MMAGANADEBIA <mmaganadebia@localhost.localdomain>
2020-03-17 18:57:16 -0600

    13m

--- mar11/address/app.js
+++ mar11/address/app.js
@@ -13,16 +13,18 @@ request({url, json: true}, (error, response) => {//nombre random
 
   var fahrenheitToCelsius = require('fahrenheit-to-celsius'); //inyeccion de dependencia de convertidor
 
-  const url = 'https://api.darksky.net/forecast/f9027dd170726ab26f9d6d0f99a467fb/19.2433,-103.725?lang=es';
+  const url = 'https://api.darkskyl.net/forecast/f9027dd170726ab26f9d6d0f99a467fb/19.2433,-103.725?lang=es';
 
   request({url, json: true}, (error, response) => { //temperatura
-    let temperatura = response.body.currently.temperature;
+    var temperatura = response.body.currently.temperature;
+    var conv=fahrenheitToCelsius(temperatura);
 
-const url = 'http://open.mapquestapi.com/geocoding/v1/address?key=KEY&location=Washington,DC'
+    const url = 'http://open.mapquestapi.com/geocoding/v1/address?key=KEY&location=Washington,DC'
 
     request({url, json: true}, (error,response ) =>{ //maquest api
-      var location = results;
-      console.log(`${responseB.name} vive en la calle ${calle} y esta a ${fahrenheitToCelsius(temperatura)} ${location}`);
+      //var location = respons.body;//
+      //console.log (location);
+      console.log(`${responseB.name} vive en la calle ${calle} y esta a ${conv}`);
     })
 
 
